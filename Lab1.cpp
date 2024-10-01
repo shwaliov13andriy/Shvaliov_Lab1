c4761e0e4dc3a4d644c11918c591e79450086348
shwaliov13andriy <andrei.shwalev@gmail.com>
2024-10-01 22:18:20 +0300

    Create Lab1.cpp

---
+++ Lab1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+std::cout<<"Hello Student!";
+std::cin>>a;
+if(a>=60){
+std::cout<<"You passed";
+}
+else {
+std::cout<<"bye bye...";
+}
+return 0;
+}
